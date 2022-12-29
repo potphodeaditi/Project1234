5498935e2cb2ceeebe32d75ca74c92be936aee53
Administrator <Administrator@DESKTOP-A6MNN4D>
2022-12-29 22:52:51 +0530

    commit

---
+++ Anudipaditi/src/string/Aditi.java
@@ -0,0 +1,12 @@
+package string;
+
+public class Aditi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s3="adii";
+		System.out.println(s3.indexOf("i"));
+
+	}
+
+}
